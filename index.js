11831e5c96c094ff6f09d69c681b58f067124530
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2022-02-22 22:04:51 +0100

    JS code formatted and started coding cart

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-var listReviews = [
-    'Ďakujem za možnosť kúpi tejto knihy. Zmenil mi život a pohľad na niektoré veci. Určite si kúpim aj ďalšiu sériu (ak bude) ❤️',
-    'Práve som začala svoju kariéru ako grafička, a môžem si povedať, že som fakt veľmi negatívna, a väčšinou práve to mi pokazí všetko. Sledujem ťa na tvojom Instagrame, a uvidela som že si vydala svoju vlastnú knihu. Tak som si povedala že si to kúpim, hlavne kvôli tým krásnym ilustráciám.... Najprv som neverila tomu, že by mi naozaj mohlo pomôcť, ale ako som si to čítala, a mala pred sebou popri pracovaní, úplne som videla veci inak. Tá energia tých obrázkov proste fakt vie vzbudiť pozitivitu v myslení. Len odporúčať viem 🤩',
-    'Ah milujem! Fakt fantastická kniha, a tie šálky sú tiež fantastické!',
-];
-
+//#region Helper
 function rdm(max) {
     return Math.round(Math.random() * (max - 1))
 }
+//#endregion
 
-function refreshReview() {
-    let textReview = document.getElementById('text-review');
-    let randomReview = listReviews[rdm(listReviews.length)];
-    textReview.innerText = randomReview;
+//#region Cart
+class Cart {
+    static items = [];
+
+    static addItem(item) {
+        this.items.push(item);
+    }
+
+    static addCurrentSlideshowItem() {
+
+    }
+}
+//#endregion
+
+//#region Reviews
+class Reviews {
+    static list = [
+        'Ďakujem za možnosť kúpi tejto knihy. Zmenil mi život a pohľad na niektoré veci. Určite si kúpim aj ďalšiu sériu (ak bude) ❤️',
+        'Práve som začala svoju kariéru ako grafička, a môžem si povedať, že som fakt veľmi negatívna, a väčšinou práve to mi pokazí všetko. Sledujem ťa na tvojom Instagrame, a uvidela som že si vydala svoju vlastnú knihu. Tak som si povedala že si to kúpim, hlavne kvôli tým krásnym ilustráciám.... Najprv som neverila tomu, že by mi naozaj mohlo pomôcť, ale ako som si to čítala, a mala pred sebou popri pracovaní, úplne som videla veci inak. Tá energia tých obrázkov proste fakt vie vzbudiť pozitivitu v myslení. Len odporúčať viem 🤩',
+        'Ah milujem! Fakt fantastická kniha, a tie šálky sú tiež fantastické!',
+    ];
+
+    static refresh() {
+        let textReview = document.getElementById('text-review');
+        let randomReview = this.list[rdm(this.list.length)];
+        textReview.innerText = randomReview;
+    }
 }
 
-refreshReview();
+Reviews.refresh();
+//#endregion
